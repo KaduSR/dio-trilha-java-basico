0d3da98277422c474ef27247fdfb9a00f6fbfc3c
Carlos Eduardo da Silva Ribeiro <carlos847@gmail.com>
2024-06-04 19:18:20 -0300

    SmartTV

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,35 @@
+public class SmartTv{
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void aumentarVolume(){
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+
+    public void escolherCanal(int novocanal){
+        canal = novocanal;
+    }
+    
+    public void mudandoCanal() {
+        canal++;
+        System.out.println("Canal atual " + canal);
+    }
+
+    public void voltandoCanall(){
+        canal--;
+        System.out.println("Canal atual " + canal);
+    }
+
+    public void ligar () {
+        ligada = true;
+    }
+    
+    public void desligar () {
+        ligada = false;
+    }
+}
---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,28 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+       
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Canal atual " + smartTv.canal);
+
+        smartTv.escolherCanal(13);
+
+        System.out.println("Canal atual " + smartTv.canal);
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+
+        
+        smartTv.desligar();
+       // System.out.println("Novo Status -> TV Desligada? " + smartTv.desligar);
+
+    }
+}
